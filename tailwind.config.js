528a0db18e29e3ddbd00ae047760e4ff470a357d
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-09-02 17:31:30 +0200

    Configure tailwind config with custom colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      heading: ["Bebas Neue", "sans-serif"],
+      primary: ["Roboto Condensed", "sans-serif"],
+      secondary: ["Roboto Flex", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        gray: {
+          primary: "#F7F7F7",
+          fontPrimary: "#282828",
+          fontSecondary: "#1D1D1B",
+        },
+        blue: {
+          primary: "#0147FF",
+        },
+      },
+    },
   },
   plugins: [],
 };
