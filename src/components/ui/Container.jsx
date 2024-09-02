df9b50c74a6ab99d636cc406732dda8f846b1fc8
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-09-02 17:32:18 +0200

    Create width container component

---
+++ src/components/ui/Container.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Container({ className, children }) {
+  return (
+    <div className={"container mx-auto max-w-[1440px] " + className}>
+      {children}
+    </div>
+  );
+}
+
+export default Container;
