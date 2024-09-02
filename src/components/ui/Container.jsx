aee8dfa164f4573c128df053459f187a94f02a09
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-09-02 19:43:46 +0200

    Fix Container to have a default className value

--- src/components/ui/Container.jsx
+++ src/components/ui/Container.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Container({ className, children }) {
+function Container({ className = "", children }) {
   return (
     <div className={"container mx-auto max-w-[1200px] " + className}>
       {children}
